9668a8cc4094a50dcec679fb1fceae98f17800a8
Giancarlo Forero <124214460+gianca2020@users.noreply.github.com>
2024-02-22 19:28:59 -0500

    Update flaskTest.py
    new attempt at backend programming to allow users to send emails to me via my website

--- flaskTest.py
+++ flaskTest.py
@@ -1,39 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_mail import Mail, Message
-#import os
+from flask import Flask
+from flask_mailman import Mail, EmailMessage
 
-app = Flask(__name__)
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'giancarloforero@gmail.com'
-app.config['MAIL_PASSWORD'] = '**********'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
+mail = Mail()
 
-@app.route('/home', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('page.html')
+def create_app():
+    app = Flask(__name__)
+    app.config["MAIL_SERVER"] = "smtp.fastmail.com"
+    app.config["MAIL_PORT"] = "465"
+    app.config["MAIL_USERNAME"] = "napoliee@fastmail.com"
+    app.config["MAIL_PASSWORD"] = "9jq5zzypelx8qrx2"
+    app.config["MAIL_USE_TLS"] = False
+    app.config["MAIL_USE_SSL"] = True
+    mail.init_app(app)
 
+    @app.route("/")
+    def index():
+        msg = EmailMessage(
+            "here is the title",
+            "This is my test with frameworks :)",
+            "napoliee@fastmail.com",
+            ["napoliee@fastmail.com"]
+        )
+        msg.send()
+        return "sent email..."
 
-@app.route('/submit-form', methods=['POST'])
-def submit_form():
-    print("Submitted")
-    try:
-        name = request.form['name']
-        email = request.form['email']
-        body = request.form['body']
-
-        msg = Message("Here is this email " + name,
-                      sender=app.config['MAIL_USERNAME'],
-                      recipients=[email])
-        msg.body = body
-        mail.send(msg)
-    except Exception as e:
-        print(e)
-
-    return redirect(url_for('home'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return app
